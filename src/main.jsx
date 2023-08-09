b6a7133facf6d8d290bd51f7634c2a4b8b92140b
yamoo9 <yamoo9@naver.com>
2023-08-09 13:48:31 +0900

    리액트에서 우리가 행해야할 규칙
    - JSX만 사용하세요.
    - 컴포넌트 이름은 첫글자가 항상 대문자로
    - 그래야 JSX 구문에서 사용 가능하니까

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-inner-declarations */
 /* React, ReactDOM 모듈 불러오기 및 버전 확인 -------------------------------------- */
 
@@ -115,21 +116,6 @@ const appElementJSX = (
 
   // - [ ] React 요소를 반환하는 함수 만들기
   //   1. 함수 작성 createAbbrElement()
-  function createAbbrElement(props, ...children) {
-    return React.createElement('abbr', props, ...children);
-  }
-
-  const domAbbrElement = createAbbrElement({
-    title: 'Document Object Model'
-  }, 'D', 'O', 'M');
-
-  console.log(domAbbrElement);
-  
-  const uiAbbrElement = createAbbrElement({
-    title: 'User Interface'
-  }, 'UI');
-  console.log(uiAbbrElement);
-  
   // - [ ] 인수를 전달해 재사용 가능하도록 구현
   //   2. createAbbrElement({ props: { title: '...' } }, '...');
 }
@@ -137,7 +123,66 @@ const appElementJSX = (
 
 /* React 요소 vs. 컴포넌트 -------------------------------------------------------- */
 
+// createAbbrElement 함수 만들기 (일반 JavaScript에서는 이렇게 활용)
+function createAbbrElement(title, children) {
+  return <abbr title={title}>{children}</abbr>;
+}
+
+// 아래 JSX 코드는 React 요소를 생성한다.
+  // 그런데 웹 브라우저는 아래 코드가 해석이 안되요.
+  // <abbr title="Document Object Model">DOM</abbr>
+
+  // const dom_AbbrElement = createAbbrElement(
+  //   'Document Object Model',
+  //   'DOM'
+  // );
+
+  // console.log(dom_AbbrElement);
+  
+  // const ui_AbbrElement = createAbbrElement(
+  //   'User Interface', 
+  //   'UI'
+  // );
+
+  // console.log(ui_AbbrElement);
+
+// Abbr 함수 컴포넌트 만들기 (React용)
+function Abbr(props /* { title, children } */) {
+  return (
+    <div>
+      <span>
+        <em>
+          <abbr title={props.title}>{props.children}</abbr>
+        </em>
+      </span>
+    </div>
+  );
+}
+
+// console.log(React.createElement(Abbr)); // <Abbr />
+
 // App 함수 컴포넌트 만들기
+const App = (props) => {
+  return (
+    <div className='App'>
+      <Abbr title="Document Object Model">
+        DOM
+      </Abbr>
+      <Abbr title="User Interface">
+        UI
+      </Abbr>
+    </div>
+  )
+}
+
+// normal function
+console.log(createAbbrElement(
+  'Document Object Model',
+  'DOM'
+));
+
+// JSX (대문자로 시작하는 함수)
+console.log(<App />);
 
 
 
