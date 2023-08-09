6b8cc8e444860ca7bf7494fd9dd989151a49f5c1
yamoo9 <yamoo9@naver.com>
2023-08-09 14:45:28 +0900

    컴포넌트 분리/관리 하기

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Heading from './components/Heading';
+import Description from './components/Description';
+import RenderingProcessList from './components/RenderingProcessList';
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+      <Description />
+      <RenderingProcessList />
+    </div>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,4 @@
-// import { createRoot } from '../node_modules/react-dom/client';
 import { createRoot } from 'react-dom/client';
+import App from './App';
 
-
-// App 함수 컴포넌트를 작성합니다.
-function App() {
-  // JSX 값 반환
-  return (
-    <div className='App'>
-      {/* JSX 주석 */}
-      {/* <Heading>React</Heading> */}
-      <h1>React</h1>
-      {/* <Description>...</Description> */}
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab
-        animi, labore facilis obcaecati ullam placeat molestiae. Enim,
-        consectetur laudantium.
-      </p>
-      {/* <List /> */}
-      <ul>
-        <li>list item A</li>
-        <li>list item B</li>
-        <li>list item C</li>
-      </ul>
-    </div>
-  );
-}
-
-// createRoot
-// create + Root (React DOM Root)
-// ReactDOMRoot 객체를 생성하는 함수
-createRoot(document.getElementById('root'))
-// ReactDOMRoot 객체는 render() 메서드를 사용해
-// React 요소를 실제 DOM 요소에 렌더링 한다.
-.render(
-  // App 컴포넌트를 렌더링할 수 있도록 JSX 구문을 추가합니다.
-  <App />
-  // <App></App>
-);
+createRoot(document.getElementById('root')).render(<App />);
