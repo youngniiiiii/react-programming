911ac74d4cd6f265a476b32a6749b58753d3c3a1
yamoo9 <yamoo9@naver.com>
2023-08-09 11:28:04 +0900

    학습 환경 초기화

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// React 모듈을 불러오기
+// ReactDOM 모듈을 불러오기
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// React 요소 작성 (React API)
+// React 요소 작성 (with JSX)
+
+// App 함수 컴포넌트 만들기
+
+
+// ReactDOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
+// ReactDOMRoot.render() 메서드로 App을 화면에 표시(렌더링)
+
