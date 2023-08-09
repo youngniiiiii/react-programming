36528a5e92cc03edb048b02aa7334d5d4a841e21
yamoo9 <yamoo9@naver.com>
2023-08-09 14:11:30 +0900

    React 함수 컴포넌트의 요건
    - 함수 이름은 첫글자가 대문자!
    - JSX를 반환!

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,31 @@
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
-const reactDomRoot = ReactDOM.createRoot(document.getElementById('root'));
+// React 함수 컴포넌트의 요건
+// - 함수 이름은 첫글자가 대문자!
+// - JSX를 반환!
 
-reactDomRoot.render(
-  <div>
-    <h1>React</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab animi,
-      labore facilis obcaecati ullam placeat molestiae. Enim, consectetur
-      laudantium.
-    </p>
-    <ul>
-      <li>list item A</li>
-      <li>list item B</li>
-      <li>list item C</li>
-    </ul>
-  </div>
+// App 함수 컴포넌트를 작성합니다.
+function App() {
+  // JSX 값 반환
+  return (
+    <div>
+      <h1>React</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum ab
+        animi, labore facilis obcaecati ullam placeat molestiae. Enim,
+        consectetur laudantium.
+      </p>
+      <ul>
+        <li>list item A</li>
+        <li>list item B</li>
+        <li>list item C</li>
+      </ul>
+    </div>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  // App 컴포넌트를 렌더링할 수 있도록 JSX 구문을 추가합니다.
+  <App />
+  // <App></App>
 );
-
-// JSX가 하는 일은 React 요소 생성 → 마크업 생성
-// JSX → 컴파일러(변환기) → React 요소 → ReactDOM → 실제 DOM 요소 생성 → 구조 작성
