b3f70ceda67c1f26021db86c6173c4a90f846b48
yamoo9 <yamoo9@naver.com>
2023-08-09 11:37:04 +0900

    React 요소란?
    - React.createElement
    - JSX

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,22 @@
 // React 모듈을 불러오기
+import * as React from 'react';
+
 // ReactDOM 모듈을 불러오기
+import * as ReactDOM from 'react-dom';
+
+// React 버전 확인하기
+// console.log(`React version: ${React.version}`);
+
+// ReactDOM 버전 확인하기
+// console.log(`ReactDOM version: ${ReactDOM.version}`);
 
 // React 요소 작성 (React API)
+const appElement = React.createElement('div');
+console.log(appElement);
+
 // React 요소 작성 (with JSX)
+const appElementJSX = <div></div>;
+console.log(appElementJSX);
 
 // App 함수 컴포넌트 만들기
 
