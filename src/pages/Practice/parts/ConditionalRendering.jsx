b465c84eccd3e2a93ab450a85dd3b090e28028c8
yamoo9 <yamoo9@naver.com>
2023-08-10 14:50:43 +0900

    서버에 데이터 요청/응답 상태 (시뮬레이션)

--- src/pages/Practice/parts/ConditionalRendering.jsx
+++ src/pages/Practice/parts/ConditionalRendering.jsx
@@ -2,10 +2,26 @@
 import reactImagePath from '@/assets/react.svg';
 import viteImagePath from '@/assets/vite.svg';
 
+/* -------------------------------------------------------------------------- */
+
+// 데이터 가져오기(fetch data)
+// 비동기 함수(async function)
+// RESTful API (C.R.U.D)
+
+// client → server (요청, Request)
+// client ← server (응답, Response)
+
+// 통신 상태
+// - isPending (대기 중)
+// - isLoading (로딩 중)
+// - isSuccess (데이터 요청/응답 성공 수신)
+// - isError (데이터 요청 실패)
+const status = 'isLoading';
+
 /* 컴포넌트 --------------------------------------------------------------------- */
 
 function ConditionalRendering({ imageType }) {
-    // 2-1. 조건 문 (함수 몸체(function body) 내부 사용)
+  // 2-1. 조건 문 (함수 몸체(function body) 내부 사용)
   // let imageComponent; // undefined
 
   // if (imageType === 'vite') {
@@ -17,17 +33,27 @@ function ConditionalRendering({ imageType }) {
 
   // 2-2. 조건 식 (함수 몸체 또는 JSX 내부 사용)
 
-  // 2-2-1. 3항 연산식
-  const imageComponent = imageType === 'vite' ? (
-    <img src={viteImagePath} alt="비트" />
-  ) : (
-    <img src={reactImagePath} alt="리액트" />
-  );
+  switch(status) {
+    case 'isPending': return <p>대기 중입니다.</p>
+    case 'isLoading': return <p>로딩 중입니다.</p>
+    case 'isError': return <p>오류 발생했어요.</p>
+    default:
+  }
 
-  
+
+  // 2-2-1. 3항 연산식
+  const imageComponent =
+    imageType === 'vite' ? (
+      <img src={viteImagePath} alt="비트" />
+    ) : (
+      <img src={reactImagePath} alt="리액트" />
+    );
+
+  console.log(imageComponent);
+
   const isReactImage = imageType === 'react';
   const imageLabel = isReactImage ? 'React' : 'Vite';
-  
+
   const isShowImage = false;
   const isntShowImageLabel = false;
 
@@ -58,14 +84,12 @@ function ConditionalRendering({ imageType }) {
           {/* <img src="/react.avif" alt="리액트" /> */}
           {/* <img src="/vite.svg" alt="비트" /> */}
 
-          {
-            isShowImage && (
-              <img
-                src={isReactImage ? '/react.avif' : '/vite.svg'}
-                alt={isReactImage ? '리액트' : '비트'}
-              />
-            )
-          }
+          {isShowImage && (
+            <img
+              src={isReactImage ? '/react.avif' : '/vite.svg'}
+              alt={isReactImage ? '리액트' : '비트'}
+            />
+          )}
 
           {/* imageType이 'vite'인 경우 'Vite', 'react'인 경우 'React' 텍스트를 화면에 표시합니다. */}
           {/* { isReactImage ? 'React' : 'Vite' } */}
@@ -73,16 +97,14 @@ function ConditionalRendering({ imageType }) {
         </div>
       </dd>
       <dd style={{ marginTop: 12 }}>
-        <p>
-          spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.
-        </p>
+        <p>spinner 또는 vite 이미지가 랜덤으로 화면에 렌더링 되도록 합니다.</p>
         <div className="conditionalRendering">
           <img className="spinner" src="/spinner.svg" alt="로딩 중..." />
           <img src="/vite.svg" alt="Vite" style={{ height: 42 }} />
         </div>
       </dd>
     </>
-  )
+  );
 }
 
 export default ConditionalRendering;
