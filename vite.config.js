02f3fdd61def7647538c9b59570df4513ccee8d7
yamoo9 <yamoo9@naver.com>
2023-08-11 15:52:42 +0900

    사용자 정의 클래스 모듈 이름 설정
    - Vite 구성 (css.modules)

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { env } from 'node:process';
+
+const idDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     devSourcemap: true,
+    modules: {
+      generateScopedName: idDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
+    }
   },
   resolve: {
     alias: {
