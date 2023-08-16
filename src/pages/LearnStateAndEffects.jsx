275c0306ad8f638bf6388a5c6f3e9987c1e52d5d
yamoo9 <yamoo9@naver.com>
2023-08-16 13:51:06 +0900

    묶을 수 있는 상태 관리는 묶어서 하는 것이 좋습니다.

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -4,28 +4,35 @@ function LearnStateAndEffects() {
   // 숫자 값 상태 관리
   
   // 마우스의 x 좌표
-  const [mouseX, setMouseX] = useState(0);
+  // const [mouseX, setMouseX] = useState(0);
   // 마우스의 y 좌표
-  const [mouseY, setMouseY] = useState(0);
+  // const [mouseY, setMouseY] = useState(0);
 
-  const handlePrintMousePosition = (e) => {
-    setMouseX(e.clientX);
-    setMouseY(e.clientY);
+  // 개별 상태 관리 → 그룹(묶음) 상태 관리
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handlePrintMousePosition = ({ pageX: x, pageY: y }) => {
+    // setMouseX(e.clientX);
+    // setMouseY(e.clientY);
+    setMousePosition({ x, y });
   };
 
   // 객체 상태 관리
+  // { x: 10, y: 96 }
+  // 배열 상태 관리
+  // [10, 96]
 
   return (
     <div
       className="m-10 flex flex-col gap-2 items-start"
-      onClick={handlePrintMousePosition}
+      onMouseMove={handlePrintMousePosition}
     >
       <h2 className="text-indigo-600 text-2xl uppercase">
         상태 및 이펙트 학습하기
       </h2>
 
       <output>
-        마우스 X 좌표 : {mouseX} / 마우스 Y 좌표 : {mouseY}
+        마우스 X 좌표 : {mousePosition.x} / 마우스 Y 좌표 : {mousePosition.y}
       </output>
     </div>
   );
