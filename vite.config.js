069255a068788f41a426ba83de67a2401e218179
yamoo9 <yamoo9@naver.com>
2023-08-18 07:28:20 +0900

    vite.config.js 오타 정정

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { env } from 'node:process';
 
-const idDev = env.NODE_ENV === 'development';
+const isDev = env.NODE_ENV === 'development';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   css: {
     devSourcemap: true,
     modules: {
-      generateScopedName: idDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
+      generateScopedName: isDev ? '[name]_[local]__[hash:base64:5]' : '[hash:base64:4]'
     }
   },
   resolve: {
