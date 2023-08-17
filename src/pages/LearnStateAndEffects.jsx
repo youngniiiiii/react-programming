419a86aba58674d4ea34b63fb47b5fdf341ba480
yamoo9 <yamoo9@naver.com>
2023-08-17 09:53:57 +0900

    상태는 스냅샷처럼 작동합니다. (해설)

--- src/pages/LearnStateAndEffects.jsx
+++ src/pages/LearnStateAndEffects.jsx
@@ -1,10 +1,31 @@
+// 상태는 스냅샷 같이 작동한다.
+// 상태란? 시간의 흐름에 따라 변하는 데이터를 말한다.
+// 스냅샷은 흐르는 상태의 특정 시점에서의 데이터 조각을 말한다.
+// 스냅샷은 현재 실행된 함수 몸체 안에서 변경이 불가능하다.
+// 렌더 트리거(요청)를 수행하는 상태 업데이트 함수에 전달한 nextState는 다음 렌더링 시점에 반영된다.
+// 그렇다면? 현재 시점에서 변경될 상태에 접근할 수는 없는가?
+// 아니다. 사이드 이펙트로 처리가 가능하다.
+// 1. 이벤트 핸들러
+// 2. useEffect 훅
+
+import { useState } from "react";
 
 function LearnStateAndEffects() {
+
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 10); // count (snapshot) = 0
+    console.log('count 값은? ', count); // 10
+  }
+  
+  
   return (
     <div className="m-10 flex flex-col gap-2 items-start">
       <h2 className={`text-indigo-600 font-suit text-2xl`}>
-        상태 및 이펙트 학습하기
+        상태 및 이펙트 학습하기 ({count})
       </h2>
+      <button type="button" onClick={handleClick}>+10</button>
     </div>
   );
 }
